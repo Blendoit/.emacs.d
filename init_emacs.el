06c8a6d5741c8c9e36fc50bd9b3fb9a732119923
blendoit <blendoit@gmail.com>
2019-09-03 19:03:01 -0700

    blendoit-ucla theme

--- init_emacs.el
+++ init_emacs.el
@@ -4,15 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode t)
- '(custom-enabled-themes (quote (blendoit)))
+ '(custom-enabled-themes (quote (blendoit-ucla)))
  '(custom-safe-themes
    (quote
-    ("4d3f9059ce9e8594351e7c99bb53bd4f618f4434bab04b185d480d7b28251936" "dd46180484782ce5001f05854af9860d0613e10c50be3922b4134d97d68b5aca" "132c52df20cd5dcee93e1ace30be8cacca8f4610138de4e61965cfc52d5e7d55" "57d746f8f0b6ca0b0be3c6cf273e73213d4eb773c6a0756fc984a3f010dee0b8" "e70654ab1649ac36ddcd7333e3a4525f706f37d608db17c3d9a89deae4dbc2da" default)))
+    ("6c3e0ab8b2a65dc980913556ffd19cd0838fdf6dd60509b7ec6964ab1bdc9a02" "22615d6e3875a2bdbcb1880352915c3420747227cc4d6189901da246f372109e" "5eeb46715f0f75190b99a17eee27e4481b34ef12d56e6eb359e69bf88b968055" "b1a9f91180ae9011bf6a8929d24e3e3aaedb44518a97eb9b6cdb3c35fdaeaf63" default)))
  '(display-line-numbers-type (quote visual))
  '(global-display-line-numbers-mode t)
- '(package-selected-packages
-   (quote
-    (magit evil-leader latex-preview-pane org-bullets evil)))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
