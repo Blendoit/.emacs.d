9282e3afc2bcfbc2fe905a62ba5e03f7f019f46f
blendoit <blendoit@gmail.com>
2019-09-03 19:31:14 -0700

    remove personal files

--- init_emacs.el
+++ init_emacs.el
@@ -10,6 +10,7 @@
     ("6c3e0ab8b2a65dc980913556ffd19cd0838fdf6dd60509b7ec6964ab1bdc9a02" "22615d6e3875a2bdbcb1880352915c3420747227cc4d6189901da246f372109e" "5eeb46715f0f75190b99a17eee27e4481b34ef12d56e6eb359e69bf88b968055" "b1a9f91180ae9011bf6a8929d24e3e3aaedb44518a97eb9b6cdb3c35fdaeaf63" default)))
  '(display-line-numbers-type (quote visual))
  '(global-display-line-numbers-mode t)
+ '(package-selected-packages (quote (org-bullets magit latex-preview-pane evil-leader)))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
