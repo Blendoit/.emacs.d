defdb394d8bdc32a28a092126001b94eabb7fb93
Blendoit <blendoit@gmail.com>
2020-07-20 17:12:55 -0700

    Custom configuration reborn.

--- init.el
+++ init.el
@@ -1,11 +1,6 @@
-(require 'package)
-(package-initialize)
+;;; init.el
 
-;; Load settings created automatically by GNU Emacs Custom.
-;; (For example, any clickable option/toggle is saved here.)
-;; Useful for fooling around with A-x customize-group <package>.
-(setq custom-file "~/.emacs.d/init_emacs.el")
-(load custom-file)
-
-;; Load settings I manually created for GNU Emacs
-(load "~/.emacs.d/init_blendoit.el")
+;; Load literate configuration file I created for GNU Emacs.
+;; The ultimate goal is to transition most of the custom settings
+;; to explicitly defined customisations.
+(org-babel-load-file "~/.emacs.d/blendoit/init-blendoit.org")
