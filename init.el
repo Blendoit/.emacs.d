f73936678c2039c91b704c778d2c83ac477ad84c
Blendoit <blendoit@gmail.com>
2021-05-15 14:36:50 -0700

    Clean up init.

--- init.el
+++ init.el
@@ -6,17 +6,9 @@
 ;;; Code:
 
 ;; First of all, we indicate the path to our literate configuration file.
-(setq my/literate-config (concat user-emacs-directory "smart-documents"))
-
-;; Emacs will startup faster next time, because it will load
-;; a byte-compiled version of our literate configuration file.
-;; (cond ((file-exists-p (concat my/literate-config ".el"))
-;;        (load my/literate-config))
-;;       ((file-exists-p (concat my/literate-config ".org"))
-;;        (org-babel-load-file (concat my/literate-config ".org")))
-;;       (t (message "No literate configuration file detected.")))
-
-(org-babel-load-file (concat my/literate-config ".org"))
+(setq my/literate-config (concat user-emacs-directory "smart-documents.org"))
+;; Then, we load this file into Emacs' memory.
+(org-babel-load-file my/literate-config)
 
 (provide 'init)
 
